f555556d3a47d3b5326e3eb9246f86116092ea51
OdairVasconcellos <124107318+OdairVasconcellos@users.noreply.github.com>
2023-02-28 21:26:35 -0300

    Add files via upload

--- Js/script.js
+++ Js/script.js
@@ -9,3 +9,13 @@ function vetor() {
 
 
 }
+
+function troca() {
+
+let resultado = [];
+for (let index = 0; index < 5; index++) {
+    resultado.push(Math.floor(Math.random() * 10));
+}
+document.getElementById("to").innerHTML = resultado;
+
+}
